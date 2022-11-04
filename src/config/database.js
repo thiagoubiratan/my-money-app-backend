813b6cad7e4eab58a19b315ecb61b813934cc976
AMCOM\\thiago.lima <thiago.lima@amcom.com.br>
2022-11-03 23:55:55 -0300

    first commit

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+mongoose.Promise = global.Promise
+module.exports = mongoose.connect('mongodb+srv://Thiago_Lima:K@micdevelop9812@mymoneyapp.5tlxrh6.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true })
+
+
+mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório."
+mongoose.Error.messages.Number.min = 
+    "O '{VALUE}' informado é menor que o limite mínimo de '{MIN}'."
+mongoose.Error.messages.Number.max = 
+    "O '{VALUE}' informado é maior que o limite máximo de '{MAX}'."
+mongoose.Error.messages.String.enum = 
+    "'{VALUE}' não é válido para o atributo '{PATH}'."
